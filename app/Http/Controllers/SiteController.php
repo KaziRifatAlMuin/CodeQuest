d5957a9fd4156c78c5c01e8eac310f176b389515
Kazi Rifat Al Muin <muin2107042@stud.kuet.ac.bd>
2025-10-05 02:12:09 +0600

    Route-Controller-View

---
+++ app/Http/Controllers/SiteController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SiteController extends Controller
+{
+    function home() {
+        return view('Home');
+    }
+    function about() {
+        return view('About');
+    }
+    function contact() {
+        return view('Contact');
+    }
+}
