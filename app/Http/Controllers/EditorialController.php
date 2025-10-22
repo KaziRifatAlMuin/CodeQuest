49d6f13fc361133ec6c59e6f34eae67e97de9d8e
Kazi Rifat Al Muin <muin2107042@stud.kuet.ac.bd>
2025-10-22 13:36:42 +0600

    Code Structure Updated

--- app/Http/Controllers/EditorialController.php
+++ app/Http/Controllers/EditorialController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Editorial;
 
 class EditorialController extends Controller
 {
--- app/Http/Controllers/ProblemController.php
+++ app/Http/Controllers/ProblemController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Problem;
 
 class ProblemController extends Controller
 {
